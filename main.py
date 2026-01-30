ac87ce91dc5e5594a1c453c74a87ca795da9a537
Sergio Pires <20127893-SergioPires2@users.noreply.replit.com>
2026-01-30 22:57:26 +0000

    Fix: Correcao do erro AsyncIO Event Loop

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import os
 from dotenv import load_dotenv
 import tempfile
 import time
+import asyncio
+
+try:
+    asyncio.get_event_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
 
 # --- IMPORTAÇÕES DA VERSÃO ESTÁVEL (v0.1.20) ---
 from langchain_google_genai import ChatGoogleGenerativeAI
